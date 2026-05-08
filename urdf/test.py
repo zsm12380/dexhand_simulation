0c04e386c0cf4026afbbfea7304669c44d61d765
zsm12380 <2681559325@qq.com>
2026-05-08 17:54:00 +0800

    new_start

--- urdf/test.py
+++
@@ -1,8 +0,0 @@
-import mujoco
-
-m = mujoco.MjModel.from_xml_path("dexhand_lh_mjcf.xml")
-d = mujoco.MjData(m)
-mujoco.mj_forward(m, d)
-
-print("Loaded OK")
-print("nbody:", m.nbody, "njnt:", m.njnt, "neq:", m.neq)
